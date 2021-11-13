d715ae35ba23de5276c9dedd470bcc99a2e011b5
Spoorthi1997 <chinivarspoorthi@gmail.com>
2021-11-13 10:20:00 +0100

    comiiting setup file

--- function.py
+++ function.py
@@ -3,4 +3,7 @@ from ipywidgets import interact,fixed
 from PIL import Image
 
 def imshow(X, resize=none):
+    img = Image.fromarray(X)
+    resize = img.resize(size=(100, 100))
+    return resize
 
